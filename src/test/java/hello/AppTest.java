2cedd43edc509a2b035fe0bebba1f9e39ee3a13c
Martin Etmajer <metmajer@getcloudnative.io>
2019-06-01 11:26:03 +0200

    Move server to port 8081 for testing.

--- src/test/java/hello/AppTest.java
+++ src/test/java/hello/AppTest.java
@@ -2,6 +2,7 @@ package hello;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
+import com.typesafe.config.ConfigFactory;
 import org.jooby.test.JoobyRule;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -13,10 +14,14 @@ public class AppTest {
    * remove the static modifier and replace the {@link ClassRule} annotation with {@link Rule}.
    */
   @ClassRule
-  public static JoobyRule app = new JoobyRule(new App());
+  public static JoobyRule app = new JoobyRule(new App().conf("application.testing.conf"));
+
+  public static final int appPort = ConfigFactory.load("application.testing.conf").getInt("application.port");
 
   @Test
   public void integrationTest() {
+    given().
+      port(appPort).
     when().
       get("/").
     then().
@@ -26,6 +31,8 @@ public class AppTest {
 
   @Test
   public void nameParamIntegrationTest() {
+    given().
+      port(appPort).
     given().
       queryParam("name", "Peter").
     when().
